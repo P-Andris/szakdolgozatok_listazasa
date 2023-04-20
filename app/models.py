47995f40264baa8a8b54547060999eaa8b6276f1
P-Andris <pillerandras0714@gmail.com>
2023-04-20 12:13:30 +0200

    CSS formázás

--- app/models.py
+++ app/models.py
@@ -4,4 +4,4 @@ class Szakdoga(models.Model):
     szakdoga_neve = models.CharField(max_length = 50, unique = True, verbose_name = 'Szakdolgozat neve')
     githublink = models.CharField(max_length = 200, unique = True, verbose_name = 'GitHub link')
     oldallink = models.CharField(max_length = 200, unique = True, verbose_name = 'Oldal link')
-    tagokneve = models.TextField(verbose_name = 'Tagok neve')
+    tagokneve = models.CharField(max_length = 300, verbose_name = 'Tagok neve')
